768f3aaeb2858528242950f1cd3e33e81d9b77db
Sheli <o.shelmith@gmail.com>
2024-03-18 09:50:57 +0300

    mm

--- routes/web.php
+++ routes/web.php
@@ -75,9 +75,6 @@ Route::post('get-token', [MPESAController::class, 'getAccessToken']);
 
 //end payments
 
-
-
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
